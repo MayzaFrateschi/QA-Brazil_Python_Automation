23ac3d028ff67d8f3ea8b6fb095dd64de35af880
mayzamunhozfrateschi <mayzamunhoz@icloud.com>
2025-12-02 11:52:06 +0400

    Subindo projeto sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-754794c0-bdca-47ab-8e4c-4cee5c62789b.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
